8201e24e460641decee760c21fdd5d3d61d47c95
thedanielenrico <thedanielenrico@gmail.com>
2019-06-19 16:30:40 -0700

    Add images to project object

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Jumbotron } from "reactstrap";
+import ProjectsObj from "../api/projects";
+import ProjectCard from "../components/ProjectCard";
 
-export default function Projects() {
-    return (
-        <div>
-            <Jumbotron className="bg-transparent">This is projects</Jumbotron>
-        </div>
-    );
+// class Welcome extends React.Component
+export default class Projects extends React.Component {
+    render() {
+
+        return (
+
+            <div>
+                <Jumbotron className="bg-transparent m-5 p-4 border rounded-0">
+                    <div className="row">
+                        <h1 className="col-sm-12 text-left">Projects</h1>
+                    </div>
+                    <hr></hr>
+                    <div className="row">
+                        <ProjectCard image={ProjectsObj[0].image}></ProjectCard>
+
+                    </div>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
