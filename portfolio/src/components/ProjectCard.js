8fdaac262a838e09e6ae7b22c1b40ff5a93ac3c1
thedanielenrico <thedanielenrico@gmail.com>
2019-06-21 22:04:46 -0700

    changed target blank

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -6,11 +6,11 @@ export default function ProjectCard(props) {
         <div className="image col-md-6 p-2 m-auto" style={{ width: '18rem;' }}>
             <img src={props.image} class="card-img-top" alt="..."></img>
             <div className="link">
-                <a href={props.deployed}>
+                <a href={props.deployed} rel="noopener">
                     <ion-icon class="icon openIcon" name="open"></ion-icon>
                 </a>
 
-                <a href={props.github}>
+                <a href={props.github} rel="noopener">
                     <ion-icon class="icon gitIcon" name="logo-github"></ion-icon>
                 </a>
             </div>
