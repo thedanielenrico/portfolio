0fc3b1f5c6c5cd52713d7de0200c8dcb4051d6e2
thedanielenrico <thedanielenrico@gmail.com>
2019-06-24 13:01:47 -0700

    add title and description to project page

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -6,14 +6,23 @@ export default function ProjectCard(props) {
         <div className="image col-md-6 p-2 m-auto" style={{ width: '18rem;' }}>
             <img src={props.image} class="card-img-top" alt="..."></img>
             <div className="link">
-                <a href={props.deployed} rel="noopener">
-                    <ion-icon class="icon openIcon" name="open"></ion-icon>
-                </a>
+                <ion-icon class="icon openIcon" name="open">
+                    <a href={props.deployed} rel="noopener">
 
-                <a href={props.github} rel="noopener">
-                    <ion-icon class="icon gitIcon" name="logo-github"></ion-icon>
-                </a>
+                    </a>
+                </ion-icon>
+
+                <ion-icon class="icon gitIcon" name="logo-github">
+                    <a href={props.github} rel="noopener">
+
+                    </a>
+                </ion-icon>
             </div>
+            <div className="title ">{props.name}</div>
+            <p className="description">
+                {props.description}
+            </p>
+
         </div>
     </>
 };
